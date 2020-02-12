87cde697dd15efb46ffbeae5572632dca4fa23d6
yunhonam11 <namyun.toronto@gmail.com>
2020-02-12 04:07:08 -0500

    commit comment

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/css/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
